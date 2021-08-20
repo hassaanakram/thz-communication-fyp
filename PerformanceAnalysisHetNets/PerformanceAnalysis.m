87a4fb145bcf5a4a343f33d0bf30157477467914
tipu-zlatan <64974141+tipu-zlatan@users.noreply.github.com>
2021-08-20 12:45:01 +0500

    power for tier 2

--- PerformanceAnalysisHetNets/PerformanceAnalysis.m
+++ PerformanceAnalysisHetNets/PerformanceAnalysis.m
@@ -4,7 +4,7 @@ iterations = 1;
 sum = zeros(2,1);
 
 % DEFINING PARAMETRES
-fMBS = 2.4E9; fUAV = 2.4E9; fTHF = 0.3E12; % Hz
+fMBS = 2.4E9; fUAV = 2.4E9; fTHz = 0.3E12; % Hz
 bMBS = 20E6; bUAV = 20E6; bSC = 10E9; % Hz
 PtMBS = 40; PtUAV = 30; PtSC = 20; Pr = 1; % dBM
 b = 0.11; a = 9;
@@ -21,22 +21,21 @@ lambdaUE = 87;
 
 for i=1:iterations
     % Get Base Stations and UEs
-    [numStations, positionStations] = getBSPositions(["MBS"],...
-                                                    [2], xMax, yMax);
+    [numStations, positionStations] = getBSPositions(["MBS","SC",'UAV'],...
+                                                    [2,40,50], xMax, yMax);
     [numUE, positionUE] = getUEPositions(lambdaUE, xMax, yMax);
     
     out = sprintf('------Trial %d------\nUE: %d\nMBSs: %d\nSCs: %d\nUAVs: %d\n',...
                                                    i,...
                                                    numUE,...
-                                                   numStations('MBS'));
-                                                   %numStations('SC'),...
-                                                   %numStations('UAV'));
+                                                   numStations('MBS'),numStations('SC'),numStations('UAV'));
+                                                   
     disp (out);  
     
-    sum(1) = sum(1) + numStations('MBS');
-    %sum(2) = sum(2) + numStations('SC');
-    %sum(3) = sum(3) + numStations('UAV');
-    sum(2) = sum(2) + numUE;
+%     sum(1) = sum(1) + numStations('MBS');
+%     %sum(2) = sum(2) + numStations('SC');
+%     %sum(3) = sum(3) + numStations('UAV');
+%     sum(2) = sum(2) + numUE;
 
     
     %Plotting positions
@@ -45,30 +44,55 @@ for i=1:iterations
     %% TARGET: GET THE USERS WHO ARE WITHIN COVERAGE
     % STEP 1: CALCULATE PATH LOSS FOR EACH TIER
     % MBS
-    shadowing = random('Lognormal', 0, 5, numUE, 1);
-    posMBS = cell2mat(positionStations('MBS'));
-    distance = getDistance(positionUE, posMBS);
-    PL_MBS = pathLossMBS(fMBS, beta, distance, shadowing);
+        shadowing = random('Lognormal', 0, 5, numUE, 1);
+        posMBS = cell2mat(positionStations('MBS'));
+        distance = getDistance(positionUE, posMBS);
+        PL_MBS = pathLossMBS(fMBS, beta, distance, shadowing);
+
+        % STEP 2: GET RECEIVED POWER AT EACH UE
+        fading = nakagami(1, numUE);
+        gain = 1000; % Assuming directional antenna gain to be 10 (dBm)
+        receivedPowerMBS = receivedPower(PtMBS, 0, 0, PL_MBS);
+
+        % STEP 3: CHECK RECEIVED POWER AGAINST THRESHOLD
+        inRangeUE = positionUE(receivedPowerMBS >= Pr, :);
+        outOfRangeUE = positionUE(receivedPowerMBS < Pr, :);
+
+        % Plotting
+        figure('Name', 'In Range and Out of Range UE');
+        scatter(inRangeUE(:,1), inRangeUE(:,2), 'gd');
+        hold on
+        scatter(outOfRangeUE(:,1), outOfRangeUE(:,2), 'rd');
+        hold on
+        scatter(posMBS(:,1), posMBS(:,2), 'ko');
+        hold off
+        legend('In Range', 'Out of Range', 'MBS');
     
-    % STEP 2: GET RECEIVED POWER AT EACH UE
-    fading = nakagami(1, numUE);
-    gain = 1000; % Assuming directional antenna gain to be 10 (dBm)
-    receivedPowerMBS = receivedPower(PtMBS, gain, fading, PL_MBS);
-    
-    % STEP 3: CHECK RECEIVED POWER AGAINST THRESHOLD
-    inRangeUE = positionUE(receivedPowerMBS >= Pr, :);
-    outOfRangeUE = positionUE(receivedPowerMBS < Pr, :);
-    
-    % Plotting
-    figure('Name', 'In Range and Out of Range UE');
-    scatter(inRangeUE(:,1), inRangeUE(:,2), 'gd');
-    hold on
-    scatter(outOfRangeUE(:,1), outOfRangeUE(:,2), 'rd');
-    hold on
-    scatter(posMBS(:,1), posMBS(:,2), 'ko');
-    hold off
-    legend('In Range', 'Out of Range', 'MBS');
-    
+    % SC
+        posSC = cell2mat(positionStations('SC'));
+        distance = getDistance(positionUE, posSC);
+        absorbtion = pathLossSCAbsorbtion(distance);
+        spread = pathLossSCSpread(fTHz,distance);
+        PL_SC = pathLossSC(absorbtion,spread);
+
+        % STEP 2: GET RECEIVED POWER AT EACH UE
+        fading = nakagami(1, numUE);
+        gain = 1000; % Assuming directional antenna gain to be 10 (dBm)
+        receivedPowerSC = receivedPower(PtMBS, gain, fading, PL_SC);
+
+        % STEP 3: CHECK RECEIVED POWER AGAINST THRESHOLD
+        inRangeUE_SC = positionUE(receivedPowerSC >= Pr, :);
+        outOfRangeUE_SC = positionUE(receivedPowerSC < Pr, :);
+
+        % Plotting
+        figure('Name', 'In Range and Out of Range UE');
+        scatter(inRangeUE_SC(:,1), inRangeUE_SC(:,2), 'gd');
+        hold on
+        scatter(outOfRangeUE_SC(:,1), outOfRangeUE_SC(:,2), 'rd');
+        hold on
+        scatter(posSC(:,1), posSC(:,2), 'ko');
+        hold off
+        legend('In Range', 'Out of Range', 'SC');
     %% CALCULATING DATARATE AT EACH IN RANGE UE
     
 end
